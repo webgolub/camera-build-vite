dba1a37a514754beb026884196acc325d4bb2deb
Александр Голубенко <webgolub@gmail.com>
2023-11-22 10:35:29 +0300

    Рефакторит breadcrumbs

--- src/components/breadcrumbs/breadcrumbs.tsx
+++ src/components/breadcrumbs/breadcrumbs.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 function Breadcrumbs(): JSX.Element {
 
   return(
@@ -5,11 +7,11 @@ function Breadcrumbs(): JSX.Element {
       <div className="container">
         <ul className="breadcrumbs__list">
           <li className="breadcrumbs__item">
-            <a className="breadcrumbs__link" href="index.html">Главная
+            <Link className="breadcrumbs__link" to="/">Главная
               <svg width="5" height="8" aria-hidden="true">
                 <use xlinkHref="#icon-arrow-mini"></use>
               </svg>
-            </a>
+            </Link>
           </li>
           <li className="breadcrumbs__item"><span className="breadcrumbs__link breadcrumbs__link--active">Каталог</span>
           </li>
